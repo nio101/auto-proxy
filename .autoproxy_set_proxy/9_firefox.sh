3f4a66e23dce28cecc5f391e526df288845e09d0
nio101 <nio101@outlook.com>
2018-01-24 19:12:03 +0100

    v1.0! :)

--- .autoproxy_set_proxy/9_firefox.sh
+++ .autoproxy_set_proxy/9_firefox.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
 # run firefox with the intranet profile
-cmd="firefox --profile /home/nio/.mozilla/firefox/ko8osw3x.intranet"
-eval "${cmd}" &>/dev/null &!;
+
+
+# remove the special group entry in /etc/bash.bashrc, if it exists
+sed '/# -= autoproxy firefox config, DO NOT EDIT MANUALLY =-/,/# -= autoproxy firefox config =-/d' /etc/bash.bashrc|sudo tee /etc/bash.bashrc > /dev/null
+
+# create it again, with proper values
+sudo bash -c 'echo "# -= autoproxy firefox config, DO NOT EDIT MANUALLY =-" >> /etc/bash.bashrc'
+sudo bash -c 'echo "alias ff=\"firefox --profile /home/nio/.mozilla/firefox/ko8osw3x.intranet\"">> /etc/bash.bashrc'
+sudo bash -c 'echo "# -= autoproxy firefox config =-" >> /etc/bash.bashrc'
